6c97ed166238fb68fa9b86c3b300700705fc5263
Bryan Blaze <100726382+BryanBlaze00@users.noreply.github.com>
2022-04-27 23:03:31 -0600

    Updated, and fixed the game from being cheat-able by disabling mouse listeners.

--- src/MemoryGUIgame.java
+++ src/MemoryGUIgame.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.plaf.ColorUIResource;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -7,8 +8,7 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
-
-public class MemoryGUIgame extends JFrame implements MouseListener {
+public class MemoryGUIgame extends JFrame{
     // Private fields
     private final Random randNum = new Random();
     private final JPanel dock = new JPanel();
@@ -16,6 +16,37 @@ public class MemoryGUIgame extends JFrame implements MouseListener {
     private final ArrayList<Integer> memorize = new ArrayList<>();
     private final ArrayList<Integer> playerGuess = new ArrayList<>();
     private boolean gameover = false;
+    private final MouseListener squeak = new MouseListener() {
+        @Override
+        public void mouseClicked(MouseEvent e) { }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+            JButton source = (JButton) e.getSource();
+            for (int i = 0 ; i < buttons.length ; i++) {
+                if (source.equals(buttons[i])) {
+                    buttons[i].setForeground(Color.GREEN);
+                    playerGuess.add(i + 1);
+                }
+            }
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            JButton source = (JButton) e.getSource();
+            for (JButton button: buttons) {
+                if (source.equals(button)) {
+                    button.setForeground(Color.BLACK);
+                }
+            }
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) { }
+
+        @Override
+        public void mouseExited(MouseEvent e) { }
+    };
 
     // Default constructor
     public MemoryGUIgame( ) throws InterruptedException {
@@ -27,6 +58,7 @@ public class MemoryGUIgame extends JFrame implements MouseListener {
         initComponents();
         play();
     }
+
     // Start Program
     public static void main(String... DoMyBidding) throws InterruptedException { new MemoryGUIgame(); }
 
@@ -36,37 +68,42 @@ public class MemoryGUIgame extends JFrame implements MouseListener {
         JOptionPane.showMessageDialog(this , """
                                              Welcome to Memory GUI!
                                              Rules:
-                                             - Random numbers will be generated 1 at a time and added in sequence with the previous numbers.
-                                             - Example : First sequence = 1 #, Second = 2 #'s, Three = 3 #'s etc..
-                                             - Wait for the sequence to finish, then click on the numbers generated in sequence.
-                                             - You have 2 seconds plus the number of generated sequences to enter your sequences.
+                                             - RED Mode: Random numbers will be generated one at a time and saved in a sequence.
+                                             - GREEN Mode: Click on the numbers generated in sequential order from 'RED Mode'.
+                                             - You will have the total generated numbers in seconds to match the sequence.
+                                             - All of your number entries must match to get to the next round.
 
                                              That's it! Good Luck!""");
-    PlayAgain:
         while (!gameover) {
             generate();
             playerTurn();
             if (gameover) {
-                JOptionPane.showMessageDialog(this , "GAME OVER!\nYou memorized " + memorize.size() + " sequences." +
-                        "\n\nTry again to beat your score!");
+                JOptionPane.showMessageDialog(this , "GAME OVER!\nYou memorized " + memorize.size() + " sequences." + "\n\nTry again to beat your score!");
                 memorize.removeAll(memorize);
                 playerGuess.removeAll(playerGuess);
                 gameover = false;
-                continue PlayAgain;
             }
         }
     }
 
     // Timer to let the player make their sequences selection and compare to win, then reset guesses.
     private void playerTurn( ) throws InterruptedException {
+        for (int i = 0; i < buttons.length ; i++ ) {
+            buttons[i].addMouseListener(squeak);
+        }
+        dock.setBackground(Color.GREEN);
         TimeUnit.SECONDS.sleep(memorize.size() + 2);
+
         if (!Arrays.toString(playerGuess.toArray()).equals(Arrays.toString(memorize.toArray()))) { gameover = true; }
         playerGuess.removeAll(playerGuess);
+        for (int i = 0; i < buttons.length ; i++ ) {
+            buttons[i].removeMouseListener(squeak);
+        }
     }
 
     // Generate random numbers, and activate button sequences to memorize
     private void generate( ) throws InterruptedException {
-
+        dock.setBackground(ColorUIResource.RED);
         if (!memorize.isEmpty()) {
             for (int temp: memorize) {
                 buttons[temp - 1].setForeground(Color.RED);
@@ -76,21 +113,20 @@ public class MemoryGUIgame extends JFrame implements MouseListener {
                 TimeUnit.SECONDS.sleep(1);
             }
         }
-        int nextNum = randNum.nextInt( 0,8);
-        memorize.add(nextNum+1);
+        int nextNum = randNum.nextInt(0 , 8);
+        memorize.add(nextNum + 1);
         buttons[nextNum].setForeground(Color.RED);
         buttons[nextNum].doClick();
         TimeUnit.SECONDS.sleep(1);
         buttons[nextNum].setForeground(Color.BLACK);
-        TimeUnit.SECONDS.sleep(1);
     }
+
     // Initialize components
     private void initComponents( ) {
         dock.setLayout(new GridLayout(3 , 3 , 3 , 3));
         this.add(dock);
         for (int i = 0 ; i < buttons.length ; i++) {
             buttons[i] = new JButton();
-            buttons[i].addMouseListener(this);
             buttons[i].setText(String.valueOf(i + 1));
             buttons[i].setFont(new Font("Arial" , Font.BOLD , 100));
             buttons[i].setForeground(Color.BLACK);
@@ -99,30 +135,6 @@ public class MemoryGUIgame extends JFrame implements MouseListener {
         }
         dock.setVisible(true);
     }
-    // Color button number red, and add player's number to array on click
-    public void mousePressed(MouseEvent e) {
-        JButton source = (JButton) e.getSource();
-        for (int i = 0 ; i < buttons.length ; i++) {
-            if (source.equals(buttons[i])) {
-                buttons[i].setForeground(Color.RED);
-                playerGuess.add(i + 1);
-            }
-        }
-    }
-    // Color button number black when mouse click releases
-    public void mouseReleased(MouseEvent e) {
-        JButton source = (JButton) e.getSource();
-        for (JButton button: buttons) {
-            if (source.equals(button)) {
-                button.setForeground(Color.BLACK);
-            }
-        }
-    }
-    public void mouseClicked(MouseEvent e) {
-
-        }
-    public void mouseEntered(MouseEvent e) { }
-    public void mouseExited(MouseEvent e) { }
 }
 
 
